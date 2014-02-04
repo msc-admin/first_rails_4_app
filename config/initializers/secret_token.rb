9539d23661e5209a9d94b0f360a61624c2d01e58
Mysmilecentral Admin <mysmilecentral@gmail.com>
2014-02-04 15:16:45 -0800

    initial commit for rails 4 test app from hartl's turorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a60ba9698915470a263afb3c971321db3aba950d634986dc98c11cca542a66a32427f2f53aa1f3fc6b26df8767886991d04024f7924ab46e302bfd62c459e489'
